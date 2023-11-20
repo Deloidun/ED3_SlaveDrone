c03ae35946f47733c5de31d1a3e85e1142f58bf4
Hiu0302 <hieubom0302@gmail.com>
2023-11-20 22:52:55 +0700

    updated code for master

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <Slave_ESP_NOW.h>
+#include <ESC_and_PWM.h>
 
 #define SIGNAL_TIMEOUT 1000  // This is signal timeout in milli seconds. We will reset the data if no signal
 
